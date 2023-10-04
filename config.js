cb058b14ea2d62a20eb5e7e11afdbecaec752aee
Toxichacker0 <146699170+Toxichacker0@users.noreply.github.com>
2023-10-04 05:29:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,9 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['+233507614089', 'me', true],
+  ['+233507614089', 'TOXIC HACKER', true],
+  ['', 'Ash', true], 
   [''] 
 ] //Number of owners
 
